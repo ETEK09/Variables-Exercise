1ffafb684acbf439846f2ab0149ceb203b2187bd
Eric Caro <c.aroericm@gmail.com>
2023-06-16 11:22:36 -0500

    Chris Evans

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int y = 76;
+
+            string whoIsthis = "Robert Dinero";
+
+            char bugs = 'A';
+
+            bool fatcs = false;
+
+            double money = 2.3d;
+
+            decimal mula = .32m;
+
+            string famousActor = "Chris Evans";
+
+            int aNumber = 88;
+
+            Console.WriteLine($"It's cool how MCU made " + famousActor + " look " + aNumber + " with makeup and CGI. ");
+
+      
         }
     }
 }
